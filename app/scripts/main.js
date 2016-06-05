c4250f63c59bde9053b5d6704c2b859a7495a943
renan.gil <renan.zgil@gmail.com>
2016-06-04 21:43:21 -0300

    Primeiro commit

---
+++ app/scripts/main.js
@@ -0,0 +1,20 @@
+// main.js
+
+'use strict';
+
+var _ = require('underscore');
+var names = ['blue t-shirt', 'yellow t-shirt', 'green t-shirt'];
+
+_.each(names, function(n) {
+	console.log(n);
+});
+
+//initialize the application
+
+var todo = {};
+todo.model = require('./models/Todo');
+todo.view = require('./views/todoView');
+
+m.route('/teste', todo.view);
+
+m.module(document.getElementById("page-app"), {controller: todo.controller, view: todo.view});
